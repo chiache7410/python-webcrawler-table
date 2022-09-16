e6fc430dc866ff08d5e10f0787739104891eb877
chiache Lee <chiache7410@gmail.com>
2022-09-17 02:15:42 +0800

    1.???啣???.?舐蕃銝???

--- webGetTabel2csv.bat
+++ webGetTabel2csv.bat
@@ -3,7 +3,7 @@ if "%1%"=="" (
 echo 請代入檔案路徑
 goto end
 )
-dir htmltemp >nul
+dir htmltemp1 >nul
 if %ERRORLEVEL%==0 python webGetTabel2csv.py %1% N
 if %ERRORLEVEL%==1 python webGetTabel2csv.py %1% Y
 :end
--- webGetTabel2csv.py
+++ webGetTabel2csv.py
@@ -1,45 +1,46 @@
-import urllib.request
-import csv
-import time
 import sys
-def getHtml(url):
-    html = urllib.request.urlopen(url).read()
-    return html
-def getName(html):
-    str0 = str(html, 'utf-8')
-    str1 = str0.split('</td>')
-    str2 = str1[7].split('<td class="rt">')
-    return str2[1]
-def numberToName(number):
-    aurl = "https://ibank.tcbbank.com.tw/PIB/cb5/cb501005/CB" + number + ".faces"
-    print(aurl)
-    input("確定開始?")
-    html = getHtml(aurl)
-    return getName(html)
-def main1(filename):
+import myFun
+import csv
+def main(url, page, previouspage, notemp):
+    nowurl = url + '&page=' + page
+    print(nowurl)
+    input("確認url後才按Enter")
+    tempfilename = 'htmltemp' + page
+    if notemp == 'Y':
+        myFun.saveHtml(tempfilename, myFun.getHtml(nowurl))
+        print('重新下載')
+    html = myFun.readHtml(tempfilename)
+    data = myFun.getValue(html, '</td></tr>', '<center>')
+    data = data.split('<tr')
+    data.pop(0)
+    data = myFun.listReplace(data, '</a>')
+    lists = []
+    for l in data:
+        data = myFun.getTabel(l, '>', '</td>')
+        data = myFun.listReplace(data, '\n')
+        data = myFun.listStrip(data)
+        lists.append(data)
     data = []
-    with open(filename, 'r', newline='', encoding='utf-8-sig') as f:
-        lines = csv.reader(f)
-        for line in lines:
-            d = []
-            d.append(line[0])
-            d.append(numberToName(line[0]))
-            print(line[0])
-            time.sleep(1)
-            data.append(d)
+    for l in lists:
+        d = []
+        d.append(l[0])
+        d.append(l[1])
+        d.append(l[2])
+        data.append(d)
+    nextpage = myFun.getValue(html, '<a href="showlist.asp?page=', '&name=OEMMap&Cmd=Map&ZoomPage=searchmapbranch&Genus=twbank&GenusType=mapbranch&sqlchk=open')
+    if previouspage != nextpage:
+        print(nextpage)
+        kk = input('有進入IF，回答Y繼續')
+        if kk == 'y':
+            data = data + main(url, nextpage, page, notemp)
+    return data
+if len(sys.argv) == 3:
+    data = []
+    data = myFun.readCSV('固定不變.csv')
+    data = data + main("https://bot.map.com.tw/search_engine/showlist.asp?name=OEMMap&Cmd=Map&ZoomPage=searchmapbranch&Genus=twbank&GenusType=mapbranch&sqlchk=open", '1', '0', sys.argv[2])
     print(data)
-    with open(filename, 'w', newline='', encoding='utf-8-sig') as f:
-        w = csv.writer(f)
-        w.writerows(data)
-
-
-if len(sys.argv) == 1:
-    while True:
-        num = input('輸入分機:')
-        if num == 'q':
-            break
-        print(numberToName(num))
-elif len(sys.argv) == 2:
-    main1(sys.argv[1])
+    input("按Enter開始寫入檔案")
+    myFun.saveCsv(sys.argv[1], data)
 else:
-    print('無該選項')
+    print('無檔案路徑')
+    input("按Enter結束")
